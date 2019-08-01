36ca53a0e54df9138660d4a4692cbdbaa956bdb7
inty <interruptinuse@gmail.com>
2019-08-01 17:02:22 +0000

    dist.sh: export separately: TAR_OPTIONS, GZIP

--- dist.sh
+++ dist.sh
@@ -3,6 +3,8 @@
 set -ex
 
 export TZ=UTC
+export TAR_OPTIONS="--owner=user:1000 --group=user:1000 --numeric-owner"
+export GZIP="--rsyncable --no-name -9"
 
 autoreconf -fvi
 ./configure --host=i686-w64-mingw32 CXXFLAGS="-g0"
@@ -11,4 +13,4 @@ make
 i686-w64-mingw32-strip --strip-debug waitpid.exe
 
 make waitpid.1.html
-TAR_OPTIONS="--owner=user:1000 --group=user:1000 --numeric-owner" GZIP="--rsyncable --no-name -9" make dist
+make dist
